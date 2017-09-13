251ff02b9e6a29ec74dc5b90df5871fb3ec575b7
soolaugust <soolaugust@gmail.com>
2017-09-13 23:43:55 +0800

    add codes and exercise codes in chapter 4

---
+++ chapter4/chapter4_1.pl
@@ -0,0 +1,9 @@
+%% program 1 in chapter 4
+
+%% a example showing procedural meaning of prolog language
+
+print():-write('I have a dream'),nl,
+         write('that my four little children will one day live in a nation'),nl,
+         write('where they will not be judged bu the color of their skin'),nl,
+         write('but by the content of their character.'),nl,
+         write('This is form Martin Luther King').
